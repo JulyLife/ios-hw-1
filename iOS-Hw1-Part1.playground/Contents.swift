b94f1fd4f5479112a6070aff29103d31321a7677
JulyLife-C60 <ghanima.sabti@gmail.com>
2020-09-09 23:12:52 +0300

    Solved HW1

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -7,7 +7,7 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+var Friendname = "Salem"
 
 
 var myWeight = 60
@@ -15,11 +15,11 @@ var FriendWeight = 80.5
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = Double(myWeight) + FriendWeight
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = Double (myWeight) * (height * height)
 
 /*
 - - - -
@@ -38,18 +38,26 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Ghanima"
+var lastName1 = "Sabti"
+var email1 = "ghanima.sabti@gmail.com"
+var phoneNumber1 = "90946228"
+var age1 = 25
+var country1 = "kuwait"
+var area1 = "Mangaf"
+var password = "1234"
+var isKuwaiti = true
 
 //Person 2
-
+var SecondName2 = "Shaikha"
+var lastName2 = "Sabti"
+var Secondsemail2 = "ikha@gmail.com"
+var SecondphoneNumber2 = "97705520"
+var Secondage2 = 17
+var Secondcountry2 = "kuwait"
+var Secondarea2 = "Mangaf"
+var Secondpassword = "4321"
+var SecondisKuwaiti = true
 
 
 
@@ -60,7 +68,15 @@ var isKuwaiti = false
 
 
 //Person 3
-
+var ThirdName3 = "Kotlin"
+var ThirdlastName3 = "Weelson"
+var Thirdsemail3 = "Kotlin@hotmail.com"
+var ThirdphoneNumber3 = "2550701"
+var Thirdage3 = 25
+var Thirdcountry3 = "London"
+var Thirdarea3 = "Piccadilly"
+var Thirdpassword = "5678"
+var ThirdisKuwaiti = false
 
 
 
@@ -82,3 +98,19 @@ var isKuwaiti = false
 
  
  */
+
+var ageDifference_1_2 =  age1 == Secondage2
+var ageDifference_1_3 = age1 == Thirdage3
+var ageDifference_2_3 = Secondage2 == Thirdage3
+var ageDifference_1_2_3 = (ageDifference_1_2) || (ageDifference_1_3) || (ageDifference_2_3)
+
+var areaDifference_1_2 = area1 == Secondarea2
+var areaDifference_1_3 = area1 == Thirdarea3
+var areaDifference_2_3 = Secondarea2 == Thirdarea3
+var areaDifference_1_2_3 = (areaDifference_1_2) || (areaDifference_1_3) || (areaDifference_2_3)
+
+var ageBigger_18 = (age1 > 18) || (Secondage2 > 18) || (Thirdage3 > 18)
+var ageLess_18 = (age1 < 18) || (Secondage2 < 18) || (Thirdage3 < 18)
+var ageEquel_17 = (age1 == 17) || (Secondage2 == 17) || (Thirdage3 == 17)
+var ageNotEquel_17 = (age1 != 17) || (Secondage2 != 17) || (Thirdage3 != 17)
+
